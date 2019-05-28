c3ba939b2b16b005bc31073f2f1b987ab53fdb28
OnchariWycliffe <OnchariWycliffe@DESKTOP-NGMPCSL>
2019-05-28 17:15:42 +0300

    Added BrowserTest Class

---
+++ SeleniumJavaFramework/src/test/java/BrowserTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.seleniumhq.org/");
+
+	}
+
+}
